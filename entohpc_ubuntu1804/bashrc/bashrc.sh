bb571c961f3ebb22cb7f1f8bbfb78896e2404efc
visoca <5386765+visoca@users.noreply.github.com>
2020-09-17 18:11:36 +0100

    bashrc

---
+++ entohpc_ubuntu1804/bashrc/bashrc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# entorepo bashrc to be add to users .bash_profile
+
+cat > /opt/entorepo/.bashrc <<'EOF'
+echo
+echo "  Your account is set up to use entorepo"
+echo
+
+
+# path to executables and libraries
+export PATH=/opt/entorepo/bin:$PATH
+export PATH=/opt/entorepo/lib/bin:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/entorepo/lib/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/entorepo/lib
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/opt/entorepo/lib/lib/pkgconfig
+
+EOF
