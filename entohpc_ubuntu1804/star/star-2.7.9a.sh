1a977f0760c73d96e8e78c7b23b687c602daf310
visoca <5386765+visoca@users.noreply.github.com>
2021-10-30 05:45:40 +0100

    corrected star 2.7.9a

--- entohpc_ubuntu1804/star/star-2.7.9a.sh
+++ entohpc_ubuntu1804/star/star-2.7.9a.sh
@@ -6,7 +6,7 @@ source /opt/entorepo/.bashrc
 
 CURRDIR=$PWD
 
-rm -rf STAR-2.7.9a star-2.7.9a.tar.gz >& /dev/null
+rm -rf STAR-2.7.9a STAR-2.7.9a.tar.gz >& /dev/null
 
 wget https://github.com/alexdobin/STAR/archive/2.7.9a.tar.gz -O STAR-2.7.9a.tar.gz
 tar -xf STAR-2.7.9a.tar.gz
@@ -32,4 +32,4 @@ ln -sTf 2.7.9a current
 ln -sf /opt/entorepo/apps/star/current/bin/* /opt/entorepo/bin/
 
 cd $CURRDIR
-rm -rf STAR-2.7.9a star-2.7.9a.tar.gz
+rm -rf STAR-2.7.9a STAR-2.7.9a.tar.gz
