3954f7e6a0e923ef1c3f3978d2db73f5e590d697
visoca <5386765+visoca@users.noreply.github.com>
2021-12-02 11:27:33 +0000

    modified oases 0.2.09 k71 and k201

--- entohpc_ubuntu1804/oases/oases-0.2.09.sh
+++ entohpc_ubuntu1804/oases/oases-0.2.09.sh
@@ -10,7 +10,12 @@ rm -rf oases >& /dev/null
 
 git clone --recursive https://github.com/dzerbino/oases
 cd oases
-make CATEGORIES=24 MAXKMERLENGTH=201 OPENMP=1
+make CATEGORIES=4 MAXKMERLENGTH=71 OPENMP=1
+mv oases oases-k71
+make CATEGORIES=4 MAXKMERLENGTH=201 OPENMP=1
+mv oases oases-k201
+ln -s oases-k201 oases
+
 perl -pi -e 's/env python/env python2/g' scripts/oases_pipeline.py
 
 rm -rf /opt/entorepo/apps/oases/0.2.09 >& /dev/null
@@ -22,7 +27,7 @@ cd /opt/entorepo/apps/oases/
 rm current >& /dev/null
 ln -sTf 0.2.09 current
 
-ln -sf /opt/entorepo/apps/oases/current/oases /opt/entorepo/bin/
+ln -sf /opt/entorepo/apps/oases/current/oases* /opt/entorepo/bin/
 ln -sf /opt/entorepo/apps/oases/current/scripts/oases_pipeline.py /opt/entorepo/bin/
 
 cd $CURRDIR
