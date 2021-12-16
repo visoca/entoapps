7dcc75e29cd528da5470c6a4c69ba4fb84e26fad
visoca <5386765+visoca@users.noreply.github.com>
2021-12-16 22:33:21 +0000

    added mummer 4.0.0rc1

---
+++ entohpc_ubuntu1804/mummer/mummer-4.0.0rc1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# load entorepo
+source /opt/entorepo/.bashrc
+
+# requires TBB libraries
+# sudo apt-get install libtbb-dev
+
+
+CURRDIR=$PWD
+
+rm -rf mummer-4.0.0rc1 mummer-4.0.0rc1.tar.gz >& /dev/null
+
+wget https://github.com/mummer4/mummer/releases/download/v4.0.0rc1/mummer-4.0.0rc1.tar.gz
+tar -xf mummer-4.0.0rc1.tar.gz
+cd mummer-4.0.0rc1
+./configure --prefix=/opt/entorepo/apps/mummer/4.0.0rc1
+make -j 12
+rm -rf --prefix=/opt/entorepo/apps/mummer/4.0.0rc1
+make install
+
+cd /opt/entorepo/apps/mummer
+rm current >& /dev/null
+ln -sTf 4.0.0rc1 current
+ln -sf /opt/entorepo/apps/mummer/current/bin/* /opt/entorepo/bin/
+
+cd $CURRDIR
+
+rm -rf mummer-4.0.0rc1 mummer-4.0.0rc1.tar.g
