1228be0287ae3e9ae2ac7796515ee34f7dbf3847
Victor Soria-Carrasco <victor.soria-carrasco@jic.ac.uk>
2023-03-04 23:10:47 +0000

    updated init_repo.sh

--- entohpc_ubuntu2204/init_repo.sh
+++ entohpc_ubuntu2204/init_repo.sh
@@ -4,7 +4,9 @@
 
 DIR=$(realpath $(dirname $0))
 
-sudo mkdir -p /opt/entorepo
-sudo chown ${USER} /opt/entorepo
+sudo mkdir -p /opt/entorepo/bin
+sudo mkdir -p /opt/entorepo/lib/lib
+sudo mkdir -p /opt/entorepo/lib/include
+sudo chown -R ${USER} /opt/entorepo
 
 ${DIR}/bashrc/bashrc.sh
