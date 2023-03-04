093d68300e0515e0d57aeb5012faba85d2e64355
Victor Soria-Carrasco <victor.soria-carrasco@jic.ac.uk>
2023-03-04 01:49:36 +0000

    added picard tools 3.0.0

---
+++ entohpc_ubuntu2204/picard/picard-3.0.0.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# load entorepo
+source /opt/entorepo/.bashrc
+
+CURRDIR=$PWD
+
+rm -rf picard-3.0.0 >& /dev/null
+
+mkdir picard-3.0.0
+cd picard-3.0.0
+wget https://github.com/broadinstitute/picard/releases/download/3.0.0/picard.jar
+
+# wrapper
+cat > picard.sh << 'EOF'
+#!/bin/bash
+
+# Picard tools wrapper
+
+# $1 -> picard tool (name jar file)
+# $2 .. $n -> rest of arguments
+
+
+# export LD_LIBRARY_PATH=/usr/local/extras/Genomics/apps/java/current/lib:$LD_LIBRARY_PATH
+
+if [[ -z $MEM ]];
+then
+	MEM=3 # memory in GB
+fi
+
+if [ -h $0 ]; then
+	DIR=$(dirname $(readlink -m $0))
+else
+	DIR=$(dirname $0)
+fi
+
+if [[ $# -eq 0 || $1 == "-h" || $1 == "--help" || $1 == "-help" ]]; then
+	echo
+	echo "This is a wrapper to use picard tools with the simple command 'picard'."
+	echo
+	echo "  Maximum Java heap size (Xmx) set to $MEM""g"
+	echo "  You can change this value with: MEM=<memory in GB> picard <options>"
+	echo "    example: MEM=6 picard MarkDuplicates I=in.bam O=out.bam M=markdupmetrics.txt"
+	echo
+	echo "Usage: picard <picard tool> <arguments>"
+	echo
+	echo "Available tools:"
+	echo
+	java -jar $DIR/picard.jar -h |& grep -vP "USAGE|Available"
+	echo
+	exit
+fi
+
+
+CHK=$(java -jar picard.jar $1 |& grep -ch "not a valid command")
+if [ $CHK -eq 1 ]; then
+	echo
+	echo "$1 is not a picard tool! Use 'picard -h' to see available tools."
+	echo
+	exit
+fi
+
+echo
+echo "Maximum Java heap size (Xmx) set to $MEM""g"
+echo
+java -Xmx"$MEM"g -jar $DIR/picard.jar $*
+
+EOF
+chmod +x picard.sh
+ln -s picard.sh picard
+
+
+rm -rf /opt/entorepo/apps/picard/3.0.0 >& /dev/null
+
+mkdir -p /opt/entorepo/apps/picard/3.0.0
+cp -r * /opt/entorepo/apps/picard/3.0.0
+cd /opt/entorepo/apps/picard
+rm current >& /dev/null
+ln -sTf 3.0.0 current
+ln -sf /opt/entorepo/apps/picard/current/picard /opt/entorepo/bin/
+
+cd $CURRDIR
+rm -rf picard-3.0.0
