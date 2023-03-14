690d868869210126bf11cffadcb1ac12d97b9401
Victor Soria-Carrasco <victor.soria-carrasco@jic.ac.uk>
2023-03-14 12:52:02 +0000

    fixed error in mash_sketch_refseq.sh

--- entohpc_ubuntu2204/scripts/mash_sketch_refseq.sh
+++ entohpc_ubuntu2204/scripts/mash_sketch_refseq.sh
@@ -3,6 +3,8 @@
 # This script creates a mash sketch library using RefSeq genomes
 # RefSeq genomes can be downloaded using the script download_refseq_genomes.sh
 
+NCPUS=24
+
 REFSEQ_VERSION=216
 
 BASE_DIR='/fastdata/db'
@@ -13,12 +15,13 @@ REFSEQ_DIR="${BASE_DIR}/RefSeq${REFSEQ_VERSION}"
 # File where paths to individual genome fasta files will be stored (output)
 REFSEQ_LIST="${BASE_DIR}/refseq_${REFSEQ_VERSION}_fnames.txt"
 
+# k-mer size and sketch sizr
+K=21
+S=1000
+
 # Mash sketch library (output)
 MSHLIB="${BASE_DIR}/refseq_${REFSEQ_VERSION}_k${K}_s${S}.msh"
 
-NCPUS=24
-K=21
-S=1000
 
 # Get paths to genomes
 find ${REFSEQ_DIR} -name "*.fna.gz" | sort -V > ${REFSEQ_LIST}
